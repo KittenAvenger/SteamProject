5b0071206e2a5878018727b3b926f12a2e72adb2
KittenAvenger <nevadaa51@yahoo.se>
2014-12-18 04:16:01 +0100

    Create mostplayed.php

---
+++ src/website/mostplayed.php
@@ -0,0 +1,316 @@
+
+<?php
+
+error_reporting(E_ALL);
+require_once('riak-php-client/src/Basho/Riak/Riak.php');
+require_once('riak-php-client/src/Basho/Riak/Bucket.php');
+require_once('riak-php-client/src/Basho/Riak/Exception.php');
+require_once('riak-php-client/src/Basho/Riak/Link.php');
+require_once('riak-php-client/src/Basho/Riak/MapReduce.php');
+require_once('riak-php-client/src/Basho/Riak/Object.php');
+require_once('riak-php-client/src/Basho/Riak/StringIO.php');
+require_once('riak-php-client/src/Basho/Riak/Utils.php');
+require_once('riak-php-client/src/Basho/Riak/Link/Phase.php');
+require_once('riak-php-client/src/Basho/Riak/MapReduce/Phase.php');
+
+
+
+function today(){
+    $client = new Basho\Riak\Riak('54.68.217.138', 8098);
+
+
+$bucket = $client->bucket('maphours');
+$start    = new DateTime(date('Y-m-d'));
+
+$year= $start->format("[Y,");
+ $month= $start->format("m,");
+    $day= $start->format("d]");
+    $new= ltrim($month,"0");
+    $new2= ltrim($day,"0");
+   
+   $date= $year . $new . $new2;
+ 
+ $obj = $bucket->getBinary($date);
+$data= $obj -> getData();
+echo(json_encode($data));
+}
+
+
+
+
+
+
+function yesterday(){
+    $client = new Basho\Riak\Riak('54.68.217.138', 8098);
+
+
+$bucket = $client->bucket('maphours');
+$start    = new DateTime(date('d.m.Y',strtotime("-1 days")));
+
+$year= $start->format("[Y,");
+ $month= $start->format("m,");
+    $day= $start->format("d]");
+    $new= ltrim($month,"0");
+    $new2= ltrim($day,"0");
+   
+   $date= $year . $new . $new2;
+ 
+ $obj = $bucket->getBinary($date);
+$data= $obj -> getData();
+echo(json_encode($data));
+}
+
+function week(){
+    $client = new Basho\Riak\Riak('54.68.217.138', 8098);
+
+
+$bucket = $client->bucket('maphoursweek');
+$start    = new DateTime(date('Y-m-d'));
+
+$year= $start->format("[Y,");
+ $month= $start->format("m,");
+    $day= $start->format("d]");
+    $new= ltrim($month,"0");
+    $new2= ltrim($day,"0");
+   
+   $date= $year . $new . $new2;
+ 
+ $obj = $bucket->getBinary($date);
+$data= $obj -> getData();
+echo(json_encode($data));
+}
+
+?>
+
+
+
+<html>
+<head><script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+<script src="highstock.js"></script>
+<script src="http://code.highcharts.com/stock/modules/exporting.js"></script>
+
+<div id="container" style="height: 400px; width: 1000px"></div>
+
+<button id="Yesterday">Yesterday</button>
+<button id="Today">Today</button>
+<button id="Week">Last week</button>
+
+</head>
+<body>
+
+
+<script>
+
+
+function gamestoday (){
+        var myVariable = <?php echo today();?>;
+        var myString3 = String(myVariable);
+        var res = myString3.replace(/(\u005B)/g, "");
+        var res2= res.replace(/(\u005D)/g, "");
+        var res3= res2.replace(/(\u007B)/g, "");
+        var res4= res3.replace(/(\u007D)/g, "");
+        var array = res4.split(',');
+        var arrayLength = array.length;
+        var games = [];
+        var hours = [];
+        for (var i = 0; i < arrayLength; i++) {
+        
+        if (i%2 == 0) {
+        games.push(array[i]);
+        } 
+        else {
+        hours.push(parseInt(array[i]));
+        }
+    
+        }
+
+        return games;
+}
+
+function hourstoday (){
+        var myVariable = <?php echo today();?>;
+        var myString3 = String(myVariable);
+        var res = myString3.replace(/(\u005B)/g, "");
+        var res2= res.replace(/(\u005D)/g, "");
+        var res3= res2.replace(/(\u007B)/g, "");
+        var res4= res3.replace(/(\u007D)/g, "");
+        var array = res4.split(',');
+        var arrayLength = array.length;
+        var games = [];
+        var hours = [];
+        for (var i = 0; i < arrayLength; i++) {
+        
+        if (i%2 == 0) {
+        games.push(array[i]);
+        } 
+        else {
+        hours.push(parseInt(array[i]));
+        }
+    
+        }
+
+        return hours;
+}
+
+function gamesyesterday (){
+        var myVariable = <?php echo yesterday();?>;
+        var myString3 = String(myVariable);
+        var res = myString3.replace(/(\u005B)/g, "");
+        var res2= res.replace(/(\u005D)/g, "");
+        var res3= res2.replace(/(\u007B)/g, "");
+        var res4= res3.replace(/(\u007D)/g, "");
+        var array = res4.split(',');
+        var arrayLength = array.length;
+        var games = [];
+        var hours = [];
+        for (var i = 0; i < arrayLength; i++) {
+        
+        if (i%2 == 0) {
+        games.push(array[i]);
+        } 
+        else {
+        hours.push(parseInt(array[i]));
+        }
+    
+        }
+
+        return games;
+}
+
+function hoursyesterday (){
+        var myVariable = <?php echo yesterday();?>;
+        var myString3 = String(myVariable);
+        var res = myString3.replace(/(\u005B)/g, "");
+        var res2= res.replace(/(\u005D)/g, "");
+        var res3= res2.replace(/(\u007B)/g, "");
+        var res4= res3.replace(/(\u007D)/g, "");
+        var array = res4.split(',');
+        var arrayLength = array.length;
+        var games = [];
+        var hours = [];
+        for (var i = 0; i < arrayLength; i++) {
+        
+        if (i%2 == 0) {
+        games.push(array[i]);
+        } 
+        else {
+        hours.push(parseInt(array[i]));
+        }
+    
+        }
+
+        return hours;
+}
+
+
+
+function lastweek (){
+        var myVariable = <?php echo week();?>;
+        var myString3 = String(myVariable);
+        var res = myString3.replace(/(\u005B)/g, "");
+        var res2= res.replace(/(\u005D)/g, "");
+        var res3= res2.replace(/(\u007B)/g, "");
+        var res4= res3.replace(/(\u007D)/g, "");
+        var array = res4.split(',');
+        var arrayLength = array.length;
+        var games = [];
+        var hours = [];
+        for (var i = 0; i < arrayLength; i++) {
+        
+        if (i%2 == 0) {
+        games.push(array[i]);
+        } 
+        else {
+        hours.push(parseInt(array[i]));
+        }
+    
+        }
+
+        return hours;
+}
+
+var chart1 = new Highcharts.Chart({
+chart: {
+            renderTo: 'container',
+            type: 'column'
+        },
+        title: {
+            text: 'Most played games'
+        },
+        subtitle: {
+            text: 'Source: Steam'
+        },
+        xAxis: {
+            categories: ['League of legends', 'Dota2', 'Wow', 'Heroes of newerth','Smite','Defense of the ancients','Bloodline Champions','Grand theft Auto', 'Half life',
+'Far cry'],
+            title: {
+                text: null
+            }
+        },
+        yAxis: {
+            min: 0,
+            title: {
+                text: 'Hours',
+                align: 'high'
+            },
+            labels: {
+                overflow: 'justify'
+            }
+        },
+        tooltip: {
+            valueSuffix: ' hours'
+        },
+        plotOptions: {
+            bar: {
+                dataLabels: {
+                    enabled: true
+                }
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'top',
+            x: -40,
+            y: 100,
+            floating: true,
+            borderWidth: 1,
+            backgroundColor: ((Highcharts.theme && Highcharts.theme.legendBackgroundColor) || '#FFFFFF'),
+            shadow: true
+        },
+        credits: {
+            enabled: false
+        },
+        series: [{
+            name: 'Hours played',
+            data: [700, 681, 650, 642, 610,600,520,505,450,400]
+        }]
+    });
+       
+   
+  $('#Yesterday').click(function() {
+    chart1.xAxis[0].setCategories(gamesyesterday());
+    chart1.series[0].setData(hoursyesterday());
+});  
+
+
+$('#Today').click(function() {
+    chart1.xAxis[0].setCategories(gamestoday());
+    chart1.series[0].setData(hourstoday());
+});
+
+$('#Week').click(function() {
+    chart1.xAxis[0].setCategories(gamestoday());
+    chart1.series[0].setData(lastweek());
+});
+
+
+function graph() {
+    chart1.xAxis[0].setCategories(gamestoday());
+    chart1.series[0].setData(hourstoday());
+}
+graph();  
+
+</script>
+</body>
+</html>
